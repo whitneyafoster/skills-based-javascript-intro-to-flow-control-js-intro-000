141482f5771c42aef0ed1cd20bc102cc9b1e305e
Whitney <foster.whitney.a@gmail.com>
2019-03-19 01:17:45 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,22 +1,22 @@
 function basicTeenager(age) {
 
 if (age >=13  && age <=19)
-(return "You are a teenager!")
+(return "You are a teenager!";)
 }
 
 function teenager(age) {
 if (age >=13 && age <=19)
 {return "You are a teenager!"}
-else {return "You are not a teenager!"};
+else {return "You are not a teenager!";}
 }
 
 function ageChecker(age) {
 if (age >=13 && age<=19)
-{return "You are a teenager!"}
+{return "You are a teenager!";}
 else if (age <=12)
-{return "You are a kid"}
+{return "You are a kid";}
 else (age >=19)
-{return "You are a grownup"}
+{return "You are a grownup";}
 }
 
 
@@ -25,5 +25,6 @@ return (age >=13 && age <=19) ? "You are a teenager!" : "You are not a teenager.
 }
 
 function switchAge(age) {
+  
 
 }
