7a3081be491d87970a51ff57c16939fface400a8
Whitney <foster.whitney.a@gmail.com>
2019-03-19 01:21:33 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,7 +6,7 @@ if (age >=13  && age <=19)
 
 function teenager(age) {
 if (age >=13 && age <=19)
-{return "You are a teenager!"}
+{return "You are a teenager"}
 else {return "You are not a teenager"}
 }
 
