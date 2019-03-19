264623d846b11c44b454241ff51ec3eb09c1b177
Whitney <foster.whitney.a@gmail.com>
2019-03-19 01:11:28 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -18,14 +18,9 @@ else if (age <=12)
 else (age >=19)
 {return "You are a grownup"}
 
-}
-
-
-}
-}
 
 function ternaryTeenager(age) {
-
+return (age >=13 && age <=19) ? "You are a teenager!" : "You are not a teenager."
 }
 
 function switchAge(age) {
